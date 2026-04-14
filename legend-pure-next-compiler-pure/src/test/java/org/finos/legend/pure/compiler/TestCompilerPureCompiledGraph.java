dbd2953e16c2149c179f87b44562b5f74d1c1f55
Pierre De Belen <pierre.debelen@gmail.com>
2026-04-14 11:23:32 -0400

    Enable more tests

--- legend-pure-next-compiler-pure/src/test/java/org/finos/legend/pure/compiler/TestCompilerPureCompiledGraph.java
+++ legend-pure-next-compiler-pure/src/test/java/org/finos/legend/pure/compiler/TestCompilerPureCompiledGraph.java
@@ -114,10 +114,9 @@ public class TestCompilerPureCompiledGraph
                                   return;
                               }
 
-                              // Tests excluded — require relation support, further error propagation, or variance checking
+                              // Tests excluded — require relation support, variance detection, or error count convergence
                               if (testName.contains("generics/relation/") ||
                                   (testName.contains("generics/") && (
-                                      testName.contains("classTypeParam_E_wrongType.pure") ||
                                       testName.contains("foldSubtypeInit_E_varianceMismatch.pure") ||
                                       testName.contains("lambdaCollectionAsParameters_E_diffParamsCount.pure")
                                   )))
